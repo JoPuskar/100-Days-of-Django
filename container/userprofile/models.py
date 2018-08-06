3c72e4d9ca4d34e4b03e2afc93b931083c47559d
JoPuskar <puskarjoshi22@gmail.com>
2018-08-06 14:44:52 +0545

    userprofile app and profile model

---
+++ container/userprofile/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(blank=True, upload_to='users/profile_pic/')
+    bio = models.TextField(max_length=200, blank=True)
+    address = models.CharField(max_length=120, blank=True)
+
+    def __str__(self):
+        if self.user.first_name and self.user.last_name:
+            full_name = self.user.first_name +" "+ self.user.last_name
+            return full_name
+        return self.user.username
+
+    @property
+    def get_fullname(self):
+        return self.user.first_name + " "+ self.user.last_name
